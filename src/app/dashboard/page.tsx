4a55ee6ce3cc239e30280d9ed37595f2b6fefaa5
Zhminko Roman <zhminkors@gmail.com>
2023-10-02 23:10:39 +0200

    feat: add cube rotating

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { useRef } from "react";
+
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { Canvas, useLoader, extend } from "@react-three/fiber";
+import { Canvas, useLoader, extend, useFrame } from "@react-three/fiber";
 import styles from "../page.module.css";
 import {
   OrbitControls,
@@ -46,42 +48,60 @@ const PixelMaterial = shaderMaterial(
   `
 );
 
-export default function Dashboard() {
+const colors = "#3d5a80 // #98c1d9 // #e0fbfc // #ee6c4d //#293241";
+
+const Box = () => {
+  const ref = useRef<THREE.Mesh>(null!);
   const modelPath = "/public/2.glb";
 
+  useFrame((state, delta) => (ref.current.rotation.y += delta));
+
   const gltf = useLoader(GLTFLoader, modelPath);
-  const material = new PixelMaterial();
+  // const material = new PixelMaterial();
+  const material = new THREE.MeshStandardMaterial({
+    color: "#ee6c4d",
+  });
   // const shaderMaterial = new THREE.ShaderMaterial();
   const geometry = new THREE.BoxGeometry(1, 1, 1);
 
   return (
+    <mesh
+      ref={ref}
+      position={[0, 0, 0]}
+      rotation={[0.3, 0.8, 0]}
+      geometry={geometry}
+      material={material}
+    >
+      {/* <pixelMaterial color="hotpink" pixelSize={8} /> */}
+      {/* <shaderMaterial
+    fragmentShader={fragmentShader}
+    vertexShader={vertexShader}
+  /> */}
+    </mesh>
+  );
+};
+
+export default function Dashboard() {
+  return (
     <main className={styles.dashboard}>
       <Canvas className={styles.scene}>
-        {/* <hemisphereLight /> */}
+        <hemisphereLight color={"#e0fbfc"} groundColor={"#3d5a80"} />
         <directionalLight color="white" position={[3, 2, 1]} intensity={1} />
         <OrbitControls />
         <PerspectiveCamera
           far={1000}
-          near={1}
-          fov={45}
+          near={0.1}
+          fov={75}
           makeDefault
-          position={[0, 0, 5]}
+          position={[0, 0, 3]}
         />
-        <mesh
-          position={[0, 0, 0]}
-          rotation={[0.3, 0.8, 0]}
-          geometry={geometry}
-          material={material}
-        >
-          {/* <pixelMaterial color="hotpink" pixelSize={8} /> */}
-          {/* <shaderMaterial
-            fragmentShader={fragmentShader}
-            vertexShader={vertexShader}
-          /> */}
-        </mesh>
+        <Box />
       </Canvas>
       <div className={styles.text}>
         <p className={styles.text_description}>Welcome to my CV website</p>
+        <p className={styles.text_description}>
+          I am Roman - frontend developer
+        </p>
       </div>
     </main>
   );
